acb7ba08a37bb1ba03936c6c9ac94c65616449bc
unknown <alekssd2005@gmail.com>
2024-11-22 10:07:53 +0200

    first commit

---
+++ uzdevumi/basics.js
@@ -0,0 +1,14 @@
+// const personAge = 18;
+// if  (personAge >= 18) {
+//     console.log("Pilngadīgs!")
+// }
+// else {
+//     console.log("Nepilngadīgs!")
+// }
+
+// let namesArray = ["Jānis", "Anna", "Zaiga"];
+// console.log(namesArray);
+
+// function sayHello() {
+//     console.log(sayHello + namesArray);
+// }
